fb761431867cded882af14847e480500583f216c
Prince2024-des <princeawlavi83@gmail.com>
2024-11-27 08:50:19 +0000

    done

---
+++ script.js
@@ -0,0 +1,51 @@
+const billAmount = document.getElementById("billAmount");
+
+const customTipAmount = document.getElementById("customTipAmount");
+const splitCount = document.getElementById("splitCount");
+
+// const submitBtn = document.getElementById("")
+
+
+const totalAmount = document.getElementById("totalAmount");
+const tipAmount = document.getElementById("tipAmount");
+const amountPerPerson = document.getElementById("amountPerPerson");
+
+
+function submit() {
+       const bill = parseFloat(billAmount.value);
+       const tip = parseFloat(customTipAmount.value);
+       const numberOfPeople = splitCount.value;
+       if(!bill || !tip || !numberOfPeople){
+        alert("some fields are empty");
+    
+       }else {
+        const totalBill = parseFloat(bill + tip);
+        const amountPerHead = parseFloat(totalBill / numberOfPeople);
+
+        totalAmount.innerHTML = `The total amount is: ${totalBill}`;
+        tipAmount.innerHTML = `Your tip is ${tip}`;
+        amountPerPerson.innerHTML = `Each person pays: ${ amountPerHead}`;
+       }
+       // reload()
+}
+
+
+function reload() {
+       billAmount.value="";
+       customTipAmount.value="";
+       splitCount.value = "";
+       // result.value = "";
+}
+
+
+function toggleDivVisibility() {
+       const serviceRatingRadioButton = document.getElementById("serviceRating");
+       const serviceRatingInputDiv = document.getElementById("serviceRatingInput");
+
+       if(serviceRatingRadioButton.checked){
+              serviceRatingInputDiv.style.display = "block"
+       }else{
+              serviceRatingInputDiv.style.display = "none"
+       }
+}
+
